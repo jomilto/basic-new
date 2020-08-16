01ca0a042e499df0e4d993e1834e51d00d301f0f
JLob Lap <joselobatof@gmail.com>
2020-08-16 15:29:30 -0500

    generando factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        App\User::create([
+            'name' => 'Jomilto F',
+            'email' => 'admin@email.com',
+            'password' => bcrypt('admin')
+        ]);
+
+        factory(App\Post::class,24)->create();
     }
 }
